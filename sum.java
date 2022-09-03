4cf38e9a0622e65c476128e20bba5d8c1a111dcd
Amit Gupta <amit770185@gmail.com>
2022-09-03 13:37:47 +0530

    First Commit

---
+++ sum.java
@@ -0,0 +1,10 @@
+public class sum
+{
+    public static void main(String args[])
+    {
+        int a=900;
+        int b=100;
+        int c=a+b;
+        System.out.println("The Sum of two numbers : "+c);
+    }
+}
